8d89586f2d70aa5a7196e9f6f390f518dc00c2f8
Booker Ngoon <bookerngoon@gmail.com>
2024-04-17 18:51:46 -0400

    added mern project

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -39,7 +39,23 @@ function Projects() {
             <h3 className="project__name">Project 3 hobbyr</h3>
           </div>
           <div className="project__description">
-          <p>Project 3 is an advanced Social Media Platform  app built with React, Django, SQL, and CSS.</p>
+          <p>Project 3 is a Cook Book website where the user can view and create recipes, this app is build with Mongoose, Express, React, and Node.js.</p>
+          <br></br>
+            <p>This platform offers users a comprehensive social experience:</p>
+          </div>
+          <div className="project__link">
+            <a href="https://chefs-kiss-cookbook.netlify.app/" className="project btn">View Project</a>
+          </div>
+        </div>
+
+
+
+        <div className="project__data">
+          <div className="project__titles">
+            <h3 className="project__name">Project 4 hobbyr</h3>
+          </div>
+          <div className="project__description">
+          <p>Project 4 is an advanced Social Media Platform  app built with React, Django, SQL, and CSS.</p>
           <br></br>
             <p>This platform offers users a comprehensive social experience:</p>
           </div>
